6391788535cf8c85d627e870535978fdc2e25c8a
Michael MacFadden <michael@macfadden.org>
2025-08-24 17:05:27 -0500

    Remove pointer-events: none from PagePlayerComponent

--- browser-core/src/player/PagePlayerComponent.ts
+++ browser-core/src/player/PagePlayerComponent.ts
@@ -112,7 +112,6 @@ export class PagePlayerComponent {
         left: 0;
         z-index: 1000;
         overflow: hidden;
-        pointer-events: none;
       }
 
       .typing-simulator-container {
